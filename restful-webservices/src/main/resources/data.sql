1e0cf529e5d1b968f1f2e8376647cfdbbcbea8a6
anitalh <106119969+anitalh@users.noreply.github.com>
2022-06-26 14:44:34 -0700

    Checking in the restful webservice

---
+++ restful-webservices/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(id, username, description, target_date,is_done)
+values(1, 'Anita Hegde', 'Meeting At 12 PM', sysdate(), false);
+
+insert into todo(id, username, description, target_date,is_done)
+values(2, 'Anita Hegde', 'Meeting At 10 AM', sysdate(), false);
+
+insert into todo(id, username, description, target_date,is_done)
+values(3, 'Anita Hegde', 'Meet friend At 5 PM', sysdate(), false);
+
+insert into todo(id, username, description, target_date,is_done)
+values(4, 'Anita Hegde', 'Buy veggies', sysdate(), false);
